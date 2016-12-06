e29b941516c5de1e49ce7affe35511ba64047822
Pedro Costa <pedrodc23@gmail.com>
2016-12-06 16:41:17 +0000

    RFC docs

--- 2nd_Assignment/src/clientTCP.c
+++ 2nd_Assignment/src/clientTCP.c
@@ -18,24 +18,22 @@ int main(int argc, char** argv){
 
 	int	sockfd;
 	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
 	int	bytes;
-	
+
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-    
+
 	/*open an TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
     	}
 	/*connect to the server*/
-    	if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
-		   sizeof(server_addr)) < 0){
+    	if(connect(sockfd,(struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
         	perror("connect()");
 		exit(0);
 	}
@@ -46,5 +44,3 @@ int main(int argc, char** argv){
 	close(sockfd);
 	exit(0);
 }
-
-
